619433f6386d8c2289e2aecd495443b8df43ba10
jack <0dumpy-consuls@icloud.com>
2025-02-05 01:34:25 -0800

    x

--- app/page.tsx
+++ app/page.tsx
@@ -46,9 +46,9 @@ export default function Home() {
       </div>
       <div className="fixed top-32 left-1/2 transform -translate-x-1/2 text-center text-red-500">
         <span className="font-bold">READ CAREFULLY:</span> This application uses
-        Metamask and ETH contracts that are deployed on the{" "}
+        Metamask to interact with smart contracts that are deployed on the{" "}
         <span className="font-bold">OPTIMISM</span> L2 network. Make sure you
-        are sending and receiving eth on an Optimism configured address.
+        are sending and receiving eth on an address configured for Optimism.
       </div>
       <div className="flex justify-center items-center space-x-6 h-screen">
         <Card1 />
