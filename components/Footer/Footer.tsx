a30dddaebe3ff158751d40775eb7d768fb3bfd83
ChadSpider <0dumpy-consuls@icloud.com>
2025-07-09 12:52:55 -0700

    n

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -7,13 +7,13 @@ import Chatbox from "@/components/Chatbox/Chatbox";
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 w-full py-4 px-4 flex justify-between items-center bg-black">
-      <div className="justify-start">
+      <div className="w-1/3 flex justify-start">
         <ToolBox />
       </div>
-      <div className="justify-center">
+      <div className="w-1/3 flex justify-center">
         <Library />
       </div>
-      <div className="justify-end">
+      <div className="w-1/3 flex justify-end">
         <Chatbox />
       </div>
     </footer>
